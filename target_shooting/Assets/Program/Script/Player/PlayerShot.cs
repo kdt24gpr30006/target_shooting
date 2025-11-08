548aa39f8a89f2de4197ef60e102fce9f60f1404
rento <koyo@303-23>
2025-11-08 16:10:17 +0900

    ボール

--- target_shooting/Assets/Program/Script/Player/PlayerShot.cs
+++ target_shooting/Assets/Program/Script/Player/PlayerShot.cs
@@ -1,18 +1,54 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MainScene
 {
-    public class PlayerShotManager
-    {
-
-    }
+    // 当たり判定用のものビヘイビアのclass
 
 
-
+    // 玉のクラス
     public class PlayerShot
     {
+        GameObject m_shotObj;
 
+        public PlayerShot(GameObject obj)
+        {
+            m_shotObj = obj;
+        }
+
+        public void Update()
+        {
+        }
     }
 
+    // 玉のマネージャークラス
+    public class PlayerShotManager
+    {
+        // リストにする
+        List<PlayerShot> m_listShots;
+
+        // コンストラクタ
+        public PlayerShotManager(GameObject obj)
+        {
+            // サイズ分生成
+            m_listShots = new List<PlayerShot>();
+
+            ////// 後でクリックしたらに帰る
+
+            // インスタンス生成
+            m_listShots.Add(new PlayerShot(obj));
+            // インスタンス削除
+            m_listShots.Remove(m_listShots[0]);
+        }
+
+        public void Update()
+        {
+            // 玉の更新処理を回す
+            foreach (var playerShot in m_listShots)
+            {
+                playerShot.Update();
+            }
+        }
+    }
 }
--- target_shooting/Assets/Program/Script/target_shooting/MainScene.cs
+++ target_shooting/Assets/Program/Script/target_shooting/MainScene.cs
@@ -5,19 +5,19 @@ namespace MainScene
     public class MainScene : MonoBehaviour
     {
         [SerializeField]
-        GameObject m_BallPrf;
+        GameObject m_ballObj;
 
-
+        PlayerShotManager playerShotManager;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-
+            playerShotManager = GetComponent<PlayerShotManager>();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            playerShotManager.Update();
         }
 
     } // class
