06d4422a28854dc1d8f5d161ba50dd1b67c5cac2
rento <koyo@303-23>
2025-11-08 14:29:17 +0900

    MainSceneのscript作成

---
+++ target_shooting/Assets/Program/Script/Player/PlayerShot.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+namespace MainScene
+{
+    public class PlayerShotManager
+    {
+
+    }
+
+
+
+    public class PlayerShot
+    {
+
+    }
+
+}
---
+++ target_shooting/Assets/Program/Script/target_shooting/MainScene.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace MainScene
+{
+    public class MainScene : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject m_BallPrf;
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    } // class
+
+} // namespace
